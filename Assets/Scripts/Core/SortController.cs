6495a36fee55a2339d217e1fd39b20a122e6981a
Stilb0n <stilbon01@gmail.com>
2026-06-17 22:50:37 +0700

    Добавлен выбор сортировки.
    При помощи выпадающего? списка. Нужно доработать счётчик т.к. сейчас он реализован только у BubbleSorter. И ещё некоторые вещи доработать, но не помню какие.

--- Assets/Scripts/Core/SortController.cs
+++ Assets/Scripts/Core/SortController.cs
@@ -1,41 +1,65 @@
 using UnityEngine;
-
+using TMPro;
 public class SortController : MonoBehaviour
 {
-    public ArrayVisualizer visualizer; // перетащи сюда объект визуализатора в Inspector
+    public ArrayVisualizer visualizer; // перетащить сюда объект визуализатора в Inspector!!!
     public OperationCounter counter;    
+    public TMP_Dropdown algorithmDropdown;
     private ISorter sorter;   // выбранный алгоритм сортировки
     private int[] values;     // массив чисел
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
-    {
-        int size = 20;
-        values = new int[size];
+    {    
+        GenerateArray();
+    }
 
-    // Генерация случайных чисел от 1 до 20
+private void GenerateArray() 
+    {
+    int size = 20;
+    values = new int[size];
+
     for (int i = 0; i < size; i++)
     {
-        values[i] = Random.Range(1, 21); // верхняя граница не включается
+        values[i] = Random.Range(1, 21);
     }
-if (visualizer == null)
-{
-    Debug.LogError("Visualizer не назначен!");
-    return;
-}
 
-if (visualizer.barPrefab == null)
-{
-    Debug.LogError("barPrefab не назначен!");
-    return;
-}
+    if (visualizer == null)
+    {
+        Debug.LogError("Visualizer не назначен!");
+        return;
+    }
+
+    if (visualizer.barPrefab == null)
+    {
+        Debug.LogError("barPrefab не назначен!");
+        return;
+    }
+
     visualizer.CreateBars(values);
+    }
+public void StartSorting()
+{
+    switch (algorithmDropdown.value)
+    {
+        case 0:
+            Debug.Log("Выбран Bubble Sort");
+            sorter = new BubbleSorter();
+            break;
 
-    sorter = new BubbleSorter();
-    counter.ResetCounter();
-    StartCoroutine(sorter.Sort(values, visualizer,counter));
-
+        case 1:
+             Debug.Log("Выбран InsertionSorter");
+            sorter = new InsertionSorter();
+            break;
+        case 2:
+            Debug.Log("Выбран SelectionSorter");
+            sorter = new SelectionSorter();
+            break;
     }
 
+    counter.ResetCounter();
+
+    StartCoroutine(sorter.Sort(values, visualizer, counter));
+}
     // Update is called once per frame
     void Update()
     {
